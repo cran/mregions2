cfdcc8fe71739ef50bf6fcf5fbf6654ef461d4b0
Salvador Jesús Fernández Bejarano <salvador.fernandez@vliz.be>
2024-09-03 16:00:07 +0000

    version 1.1.1

---
+++ inst/doc/mregions2.R
@@ -0,0 +1,12 @@
+## ----include = FALSE----------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>",
+  fig.path = "man/figures/mregions2-",
+  out.width = "100%",
+  cache = TRUE
+)
+
+options(rdf_print_format = "turtle")
+options(rmarkdown.html_vignette.check_title = FALSE)
+
